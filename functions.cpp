ad9607b10a8fa3a6863aac8ee582cc5ab6277a34
Yann Orlarey <orlarey@gmail.com>
2022-02-21 15:22:40 +0100

    more functions

--- functions.cpp
+++ functions.cpp
@@ -1,13 +1,22 @@
 #include "functions.h"
 
 static std::map<std::string, std::function<I(I)>> gFunctions = {
-    {"square", [](const I& i) { return square(i); }}, {"sin", [](const I& i) { return sin(i); }},
-    {"cos", [](const I& i) { return cos(i); }},       {"tan", [](const I& i) { return tan(i); }},
-    {"asin", [](const I& i) { return asin(i); }},     {"acos", [](const I& i) { return acos(i); }},
-    {"atan", [](const I& i) { return atan(i); }},     {"sinh", [](const I& i) { return sinh(i); }},
-    {"cosh", [](const I& i) { return cosh(i); }},     {"tanh", [](const I& i) { return tanh(i); }},
-    {"log", [](const I& i) { return log(i); }},       {"exp", [](const I& i) { return exp(i); }},
-    {"sqrt", [](const I& i) { return sqrt(i); }},     {"abs", [](const I& i) { return abs(i); }},
+    {"square", [](const I& i) { return square(i); }},
+    {"sin", [](const I& i) { return sin(i); }},
+    {"cos", [](const I& i) { return cos(i); }},
+    {"tan", [](const I& i) { return tan(i); }},
+    {"asin", [](const I& i) { return asin(i); }},
+    {"acos", [](const I& i) { return acos(i); }},
+    {"atan", [](const I& i) { return atan(i); }},
+    {"sinh", [](const I& i) { return sinh(i); }},
+    {"cosh", [](const I& i) { return cosh(i); }},
+    {"tanh", [](const I& i) { return tanh(i); }},
+    {"log", [](const I& i) { return log(i); }},
+    {"exp", [](const I& i) { return exp(i); }},
+    {"sqrt", [](const I& i) { return sqrt(i); }},
+    {"abs", [](const I& i) { return abs(i); }},
+    {"floor", [](const I& i) { return I(floor(i.lower()), floor(i.upper())); }},
+    {"ceil", [](const I& i) { return I(ceil(i.lower()), ceil(i.upper())); }},
     {"identity", [](const I& i) { return i; }}
 
 };
